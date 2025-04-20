e5a0fb1d87872a8640b45970d8f8897789079ef7
magnus <magnus.wahlstrand@gmail.com>
2025-04-20 19:55:49 +0200

    feat(proj-4): fix build v4

--- 4-oskar-typing-game/next.config.ts
+++ 4-oskar-typing-game/next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: '.',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
